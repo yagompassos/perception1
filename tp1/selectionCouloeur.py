9a235322ea4475af98fa122170c5ae22627c283e
Yago Passos <yagompassos@gmail.com>
2025-09-09 08:39:06 +0200

    resolution du premiere exercice

--- tp1/selectionCouloeur.py
+++ tp1/selectionCouloeur.py
@@ -1,15 +1,29 @@
-#converstion de RGB pour HSV
-
 import cv2 as cv
 import sys
+import numpy as np
 
 def mouse_selection(event, x, y, flags, param):
     if event == cv.EVENT_LBUTTONDOWN:
         print(f"Mouse clicked at: ({x}, {y})")
-        pixel = img[y, x]
-        print(f"Pixel value (BGR): {pixel}")
+        pixel_rgb = img[y, x]
+        print(f"Pixel value (BGR): {pixel_rgb}")
 
-img = cv.imread(cv.samples.findFile("balle_small.jpg"))
+        cor_rgb_np = np.uint8([[pixel_rgb]])
+        cor_hsv = cv.cvtColor(cor_rgb_np, cv.COLOR_BGR2HSV)[0][0]
+        tolerancia_hsv = np.array([100, 100, 100])  # H, S, V
+        lower_bound = np.array(cor_hsv - tolerancia_hsv)
+        upper_bound = np.array(cor_hsv + tolerancia_hsv)
+
+        img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+
+        mask = cv.inRange(img_hsv, lower_bound, upper_bound)
+
+        result_img = cv.bitwise_and(img, img, mask=mask)
+
+        cv.imshow("Pixels Selecionados", result_img)
+
+
+img = cv.imread(cv.samples.findFile("../assets/balle_small.jpg"))
 
 if img is None:
     sys.exit("Could not read the image.")
@@ -20,8 +34,6 @@ cv.imshow("normal image", img)
 cv.setMouseCallback('normal image', mouse_selection)
 
 # hsv img
-img_hsv = cv.cvtColor(img, cv.COLOR_RGB2HSV)
-cv.imshow("HSV image", img_hsv)
 
 k = cv.waitKey(0)
 
