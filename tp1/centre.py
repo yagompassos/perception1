3309338b3c9a50c94d61205f349e90f74bb20c32
Yago Passos <yagompassos@gmail.com>
2025-09-09 10:18:43 +0200

    corrects valeurs du mask e params hough

--- tp1/centre.py
+++ tp1/centre.py
@@ -3,13 +3,11 @@ import numpy as np
 
 def mouse_selection(event, x, y, flags, param):
     if event == cv.EVENT_LBUTTONDOWN:
-        print(f"Mouse clicked at: ({x}, {y})")
         pixel_rgb = img[y, x]
-        print(f"Pixel value (BGR): {pixel_rgb}")
 
         cor_rgb_np = np.uint8([[pixel_rgb]])
         cor_hsv = cv.cvtColor(cor_rgb_np, cv.COLOR_BGR2HSV)[0][0]
-        
+
         tolerancia_hsv = np.array([10, 200, 100])  # H, S, V
         lower_bound = np.array(cor_hsv - tolerancia_hsv)
         upper_bound = np.array(cor_hsv + tolerancia_hsv)
@@ -20,16 +18,33 @@ def mouse_selection(event, x, y, flags, param):
 
         result_img = cv.bitwise_and(img, img, mask=mask)
 
-        cv.imshow("selecao", mask)
+        #hough circles
+
+        blur = cv.medianBlur(mask, 5)
+
+        circles = cv.HoughCircles(blur, cv.HOUGH_GRADIENT, 1, 20,
+                                    param1=100, param2=20,
+                                    minRadius=1, maxRadius=0)
+        
+        if circles is not None:
+            circles = np.uint16(np.around(circles))
+            for i in circles[0, :]:
+                center = (i[0], i[1])
+                # circle center
+                cv.circle(img, center, 1, (0, 100, 100), 3)
+                # circle outline
+                radius = i[2]
+                cv.circle(img, center, radius, (255, 0, 255), 3)
+                cv.imshow("detected circles", img)
+        else :
+            print("No circles detected")
+
 
 
 img = cv.imread(cv.samples.findFile("../assets/balle_small.jpg"))
 
-# img = cv.resize(img, (600, 800))
 
 cv.imshow("normal image", img)
 cv.setMouseCallback('normal image', mouse_selection)
 
 k = cv.waitKey(0)
-
-#haufh
