bc96fcc83da845a678f249e881523868bc1f473c
Yago Passos <yagompassos@gmail.com>
2025-10-15 22:19:23 +0200

    en cherchant les bons valeurs de tolerance

--- td1.py
+++ td1.py
@@ -1,36 +1,39 @@
 import cv2 as cv
 import numpy as np
 
-def aplly_mask(cor_hsv):
-    # Distance circulaire sur H (0..180) -> gérer le wrap
-    img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)    # on convertit l'image en HSV
+def apply_mask(img, cor_hsv):
+    img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
     H, S, V = cv.split(img_hsv)
 
-    tolerance_H = 30 # tolérance sur la teinte (0..180)
-    tolerance_S = 120  # tolérance sur la saturation (0..255)
+    tolerance_H = 50
+    tolerance_S = 120
+    tolerance_V = 170
+    
     low_h = (int(cor_hsv[0]) - tolerance_H) % 180
     high_h = (int(cor_hsv[0]) + tolerance_H) % 180
 
     if low_h <= high_h:
         mask_h = (H >= low_h) & (H <= high_h)
     else:
-        # l'intervalle traverse 0 -> union de deux morceaux
         mask_h = (H >= low_h) | (H <= high_h)
 
-    # Bande de saturation
     low_s = max(0, int(cor_hsv[1]) - tolerance_S)
     high_s = min(255, int(cor_hsv[1]) + tolerance_S)
     mask_s = (S >= low_s) & (S <= high_s)
 
-    # Masque binaire final (balle=255, fond=0)
-    mask = (mask_h & mask_s).astype(np.uint8) * 255
+    low_v = max(0, int(cor_hsv[2]) - tolerance_V)
+    high_v = min(255, int(cor_hsv[2]) + tolerance_V)
+    mask_v = (V >= low_v) & (V <= high_v)
+    
+    mask = (mask_h & mask_s & mask_v).astype(np.uint8) * 255
     return mask
 
 def apply_morphologie(mask):
     # Morfologie pour nettoyer le masque
     kernel = np.ones((5,5),np.uint8) # j'ai pris ce ligne dans la documentation d'OpenCV
-    opening = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel) # ouverture de l'image (erosion > dilatation)
-    fermeture = cv.morphologyEx(opening, cv.MORPH_CLOSE, kernel) # fermeture de l'image (dilatation > erosion)
+    opening = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel, iterations=5) # ouverture de l'image (erosion > dilatation)
+    fermeture = cv.morphologyEx(opening, cv.MORPH_CLOSE, kernel, iterations=5) # fermeture de l'image (dilatation > erosion)
+    # resultat = cv.morphologyEx(fermeture, cv.MORPH_DILATE, kernel, iterations=2)
     return fermeture
 
 def findGoodCircle(gradient):
@@ -65,15 +68,15 @@ def mouse_selection(event, x, y, flags, param):
         pixel_rgb = img[y, x]
         cor_rgb_np = np.uint8([[pixel_rgb]])    
         cor_hsv = cv.cvtColor(cor_rgb_np, cv.COLOR_BGR2HSV)[0][0]
- 
-        mask = aplly_mask(cor_hsv)
-        cv.imshow("mask", mask)
+
+        mask = apply_mask(img, cor_hsv)
+        # cv.imshow("mask", mask)
         
         morphologie = apply_morphologie(mask)
         # cv.imshow("morphologie", morphologie)
 
         gradient = cv.Laplacian(morphologie, cv.CV_8UC1)
-        # cv.imshow("mask after morphologie, blur et gradient", gradient)
+        cv.imshow("gradient", gradient)
 
         # bon_cercle = findGoodCircle(gradient) #find the first circle with HOUGH CIRCLES
 
